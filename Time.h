d72a884de2a1f79f2a95b74e231bd4f54f47b43c
ChickkenGoat <kheawkhumhassadin@gmail.com>
2026-03-16 08:27:19 +0000

    function_Caculate_Difference_Of_Time

--- Time.h
+++ Time.h
@@ -1,7 +1,25 @@
 struct Time{
-	int m,h,s;
+	int h,m,s;
 };
-
+void getTime(struct Time &t){
+	
+	cin>>t.h>>t.m>>t.s;
+}
+struct Time subtract(struct Time t2,struct Time t1){
+	struct Time t3;//t3=t2-t1
+	int sec1 = t1.h*3600 + t1.m*60 + t1.s;
+	int sec2 = t2.h*3600 + t2.m*60 + t2.s;
+	int diff = sec2 - sec1;
+	if (diff < 0) diff += 24*3600; 
+	t3.h = diff / 3600;
+	t3.m = (diff % 3600) / 60;
+	t3.s = diff % 60;
+	return t3;
+}
+void display(struct Time t){
+	cout<<"t3 ";
+	cout << t.h << ":" << t.m << ":" << t.s << endl;
+}
 
 //create 3 functions
 
--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <iomanip>
-
+#include <time.h>
 using namespace std;
 
 #include "Time.h"
 
 int main(){
     struct Time t1,t2,t3;
-    cout<<"What time was it?";
+    cout<<"t1 ";
     getTime(t1);
-    cout<<"What time is it now?";
+    cout<<"t2 ";
     getTime(t2);
-    t3=subtract(t2,t1); //t3=t2-t1
-    cout<<"Time diff is ";
+    t3=subtract(t2,t1);
     display(t3);
 }
